c4e12e0a4b3e3732f283a170c711f85317d87bdd
cubiclesoft <webmaster@cubiclesoft.com>
2013-08-17 16:50:06 -0700

    Twilio support logic for next bus times and address geolocation for nearest stops.

--- base.php
+++ base.php
@@ -1,6 +1,8 @@
 <?php
 	require_once "support/mysql.php";
 	require_once "support/utf8.php";
+	require_once "support/http.php";
+	require_once "support/web_browser.php";
 
 	$db = new MySQL("localhost", "suntran", "suntran_demo", "unrelated_suntran");
 
@@ -28,6 +30,67 @@
 		return $result;
 	}
 
+	function GetRealtimeStopIDInformation($id)
+	{
+		global $db;
+
+		try
+		{
+			$name = "";
+			$dirmap = array();
+			$stopinfo = array();
+			$id = (int)$id;
+			$result = $db->Query("SELECT * FROM stops WHERE stopid = %d", $id);
+			while ($row = $result->NextRow())
+			{
+				$stopinfo[] = array("directionID" => $row->directionid, "stopID" => $row->stopid, "timePointID" => $row->timepointid);
+				$dirmap[$row->directionid] = $row->direction;
+				$name = $row->name;
+			}
+
+			$response = array("success" => true, "info" => array());
+			$result = $db->Query("SELECT r.* FROM route_stops AS rs, routes AS r WHERE rs.routeid = r.id AND rs.stopid = %d", $id);
+			while ($row = $result->NextRow())
+			{
+				$web = new WebBrowser();
+				$web->SetState(array("referer" => "http://suntran.com/tmwebwatch/GoogleLiveMap", "autoreferer" => false));
+				$result2 = $web->Process("http://suntran.com/tmwebwatch/GoogleMap.aspx/getStopTimes", "auto", array("method" => "POST", "body" => json_encode(array("routeID" => $row->id, "stops" => $stopinfo)), "headers" => array("Content-Type" => "application/json; charset=utf-8", "Accept" => "application/json, text/javascript, */*; q=0.01", "X-Requested-With" => "XMLHttpRequest")));
+
+				if (!$result2["success"])  return $result2;
+				else if ($result2["response"]["code"] != 200)  return array("success" => false, "error" => "Error retrieving URL.  Server returned:  " . $result2["response"]["code"] . " " . $result2["response"]["meaning"], "errorcode" => "response_failure");
+
+				$data = @json_decode($result2["body"], true);
+				if (isset($data["d"]["stops"]))
+				{
+					foreach ($data["d"]["stops"] as $stop)
+					{
+						if (isset($stop["crossings"]))
+						{
+							$name2 = $name . " (" . $dirmap[$stop["directionID"]] . ")";
+							if (!isset($response["info"][$name2]))  $response["info"][$name2] = array();
+							if (!isset($response["info"][$name2]["Route #" . $row->internalid]))  $response["info"][$name2]["Route #" . $row->internalid] = array();
+
+							foreach ($stop["crossings"] as $crossing)
+							{
+								if (!$crossing["cancelled"])
+								{
+									if (isset($crossing["predTime"]))  $response["info"][$name2]["Route #" . $row->internalid][] = $crossing["predTime"] . " " . $crossing["predPeriod"];
+									else  $response["info"][$name2]["Route #" . $row->internalid][] = $crossing["schedTime"] . " " . $crossing["schedPeriod"];
+								}
+							}
+						}
+					}
+				}
+			}
+
+			return $response;
+		}
+		catch (Exception $e)
+		{
+			return array("success" => false, "error" => "A database query error or other exception occurred.", "errorcode" => "exception");
+		}
+	}
+
 	function GetNearbyStopsRealtime($lat, $lon, $limit)
 	{
 		global $db;
@@ -39,6 +102,68 @@
 		$result = $db->Query($sql);
 
 		return $result;
+	}
+
+	function GeocodeAddress($address)
+	{
+		$mapquest_key = "Fmjtd%7Cluub25utn1%2C2a%3Do5-9u80qw";
+
+		$web = new WebBrowser();
+		$result = $web->Process("http://www.mapquestapi.com/geocoding/v1/address?key=" . $mapquest_key . "&location=" . urlencode($address));
+
+		if (!$result["success"])  return $result;
+		else if ($result["response"]["code"] != 200)  return array("success" => false, "error" => "Error retrieving URL.  Server returned:  " . $result["response"]["code"] . " " . $result["response"]["meaning"], "errorcode" => "response_failure");
+
+		$data = @json_decode($result["body"], true);
+
+		$bestlocation = false;
+		$bestaccuracy = 0;
+		foreach ($data["results"] as $result)
+		{
+			foreach ($result["locations"] as $location)
+			{
+				$accuracy = substr($location["geocodeQualityCode"], 0, 3);
+				switch ($accuracy)
+				{
+					case "P1A":  $accuracy = 9;  break;
+					case "P1B":  $accuracy = 8;  break;
+					case "P1C":  $accuracy = 7;  break;
+
+					case "L1A":  $accuracy = 8;  break;
+					case "L1B":  $accuracy = 7;  break;
+					case "L1C":  $accuracy = 7;  break;
+
+					case "I1A":  $accuracy = 8;  break;
+					case "I1B":  $accuracy = 7;  break;
+					case "I1C":  $accuracy = 7;  break;
+
+					case "B1A":  $accuracy = 6;  break;
+					case "B1B":  $accuracy = 5;  break;
+					case "B1C":  $accuracy = 4;  break;
+					case "B2A":  $accuracy = 6;  break;
+					case "B2B":  $accuracy = 5;  break;
+					case "B2C":  $accuracy = 4;  break;
+					case "B3A":  $accuracy = 7;  break;
+					case "B3B":  $accuracy = 6;  break;
+					case "B3C":  $accuracy = 5;  break;
+
+					default:  $accuracy = 0;
+				}
+
+				if ($accuracy < 6)  continue;
+
+				if ($bestaccuracy < $accuracy)
+				{
+					$bestlocation = $location;
+					$bestaccuracy = $accuracy;
+				}
+			}
+		}
+
+		if ($bestaccuracy == 0)  return array("success" => false, "error" => "Unable to geocode the address.", "errorcode" => "geocoder_error");
+		else if ($bestaccuracy < 6)  return array("success" => false, "error" => "The address was unable to be geocoded with sufficient accuracy.", "errorcode" => "low_accuracy");
+
+		return array("success" => true, "geo_latitude" => $bestlocation["latLng"]["lat"], "geo_longitude" => $bestlocation["latLng"]["lng"], "geo_accuracy" => $bestaccuracy, "info" => $bestlocation);
 	}
 
 	function DisplayDateTime($ts, $date)
--- twilio.php
+++ twilio.php
@@ -2,10 +2,90 @@
 	require_once "base.php";
 	require_once "support/twilio/Twilio.php";
 
+	// Process the incoming request.  Stop number is ideal (no geocoding).
+	$messages = array();
+	if (!isset($_REQUEST["Body"]))  $messages[] = "Unknown request.  Try sending # followed by the stop number (e.g. #2888).";
+	else
+	{
+		$body = trim(preg_replace('/\s+/', " ", $_REQUEST["Body"]));
+		if (substr($body, 0, 1) == "#")
+		{
+			$id = (int)substr($body, 1);
+			$result = GetRealtimeStopIDInformation($id);
+			if (!$result["success"])  $messages[] = "Error:  " . $result["error"] . " (" . $result["errorcode"] . ")";
+			else if (!count($result["info"]))  $messages[] = "The stop #" . $id . " is unable to be found in the database.";
+			else
+			{
+				foreach ($result["info"] as $name => $routes)
+				{
+					$messages[] = $name;
+					if (!count($routes))  $messages[] = "No routes/times";
+					else
+					{
+						$routemap = array_keys($routes);
+						natcasesort($routemap);
+						foreach ($routemap as $route)
+						{
+							$messages[] = $route . " @ " . implode(", ", $routes[$route]);
+						}
+					}
+				}
+			}
+		}
+		else if (strtolower($body) == "help" || strtolower($body) == "info" || strtolower($body) == "information")
+		{
+			$messages[] = "Send a text message with # followed by a stop number to get the next three times (e.g. #2888).";
+			$messages[] = "To find the nearest bus stops to a location, send a text message with the address.";
+		}
+		else
+		{
+			// Attempt to geocode the request.
+			$address = $_REQUEST["Body"];
+			if (stripos($address, "Tucson") === false)  $address .= ", Tucson";
+			if (stripos($address, " AZ") === false)  $address .= " AZ";
+			$result = GeocodeAddress($address);
+			if (!$result["success"])  $messages[] = "Error:  " . $result["error"] . " (" . $result["errorcode"] . ")";
+			else
+			{
+				// Find the nearest stops.
+				$stops = array();
+				$result2 = GetNearbyStopsRealtime($result["geo_latitude"], $result["geo_longitude"], 10);
+				while ($row = $result2->NextRow())
+				{
+					if (!isset($stops[$row->stopid]))  $stops[$row->stopid] = array();
+					$stops[$row->stopid][] = $row;
+				}
+
+				foreach ($stops as $rows)
+				{
+					$directions = array();
+					foreach ($rows as $row)  $directions[$row->direction] = $row->direction;
+
+					$messages[] = "Stop #" . $row->stopid . ":  " . $row->name . " (" . implode(", ", $directions) . "), " . sprintf("%.2f", $row->distance) . " miles";
+
+					if (count($messages) == 3)  break;
+				}
+
+				if (!count($messages))  $messages[] = "Unable to find any stops near the specified address.  Try again later.";
+			}
+		}
+	}
+
 	$response = new Services_Twilio_Twiml();
-	$response->sms("Hi.  A response would go here.");
+	$line = "";
+	foreach ($messages as $message)
+	{
+		if (strlen($line) + strlen($message) > 135)
+		{
+			if ($line != "")  $response->sms($line);
+			$line = "";
+		}
+
+		if ($line != "")  $line .= " | ";
+		$line .= $message;
+		$line = trim(preg_replace('/\s+/', " ", $line));
+	}
+	if ($line != "")  $response->sms($line);
 
 	echo $response;
-
-//	$_REQUEST["Body"];
 ?>
